28d23d94f4d366e0ac0a39179f0e22a5c1a0319d
Chirag Pattanaik <chiragrocks02@gmail.com>
2023-06-29 13:25:09 +0530

    Path fix

--- js/link.js
+++ js/link.js
@@ -1,9 +1,14 @@
 // Get the current URL
-var currentUrl = window.location.href;
-console.log(currentUrl)
+var currentUrl = window.location.pathname;
+
+// Remove the file extension from the URL
+var urlWithoutExtension = currentUrl.replace(/\.html$/, '');
+
+// Decode the URL
+var decodedUrl = decodeURIComponent(urlWithoutExtension);
 
 // Replace %20 with spaces in the URL
-var displayedUrl = decodeURIComponent(currentUrl);
+var displayedUrl = decodedUrl.replace(/%20/g, ' ');
 
 // Update the URL in the address bar
 window.history.replaceState({}, document.title, displayedUrl);
